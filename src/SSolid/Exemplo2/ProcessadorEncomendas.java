61227ea52624f89fd93e902ec5ba92ca971dd97d
Ysadora Sistini <ysadora.sistini@edu.unifil.br>
2025-04-01 12:57:22 -0300

    Inclusão exemplo2 S Solid

--- src/SSolid/Exemplo2/ProcessadorEncomendas.java
+++ src/SSolid/Exemplo2/ProcessadorEncomendas.java
@@ -6,30 +6,22 @@ import java.util.Scanner;
 public class ProcessadorEncomendas {
 
     public void processar() {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Digite o ID da encomenda: ");
-            String idEncomenda = sc.nextLine();
+        // Criando instâncias
+        EntradaEncomenda entrada = new EntradaEncomenda();
+        CalcularFrete calcularFrete = new CalcularFrete();
+        ArquivoSalvar salvar = new ArquivoSalvar();
 
-            System.out.println("Digite o peso (em kg): ");
-            double peso = sc.nextDouble();
+        // Obtendo o ID da encomenda do usuário
+        String idEncomenda = entrada.obterId();
 
-            double valorFrete = peso * 10;
-            System.out.println("Valor do frete calculado: " + valorFrete);
+        // Obtendo o peso da encomenda do usuário
+        double peso = entrada.obterPesoEncomenda();
 
+        // Calculando o valor do frete com base no peso informado
+        double valorFrete = calcularFrete.calcularFrete(peso);
+        System.out.println("Valor do frete calculado: "+valorFrete);
 
-            salvarEmArquivo(idEncomenda, valorFrete);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void salvarEmArquivo(String idEncomenda, double valorFrete) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("encomendas.txt", true))) {
-            bw.write("ID: " + idEncomenda + " - Frete: " + valorFrete);
-            bw.newLine();
-            System.out.println("Salvo no arquivo encomendas.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Salvando os dados da encomenda no arquivo
+        salvar.salvarEmArquivo(idEncomenda, valorFrete);
     }
 }
